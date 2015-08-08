7e2d62b0e821d878d51678f1e4f45ed279effdde
FrackenToaster <frakentoaster@gmail.com>
2015-08-07 18:32:56 -0700

    Workout controller and routes

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150808005541) do
+
+  create_table "workouts", force: :cascade do |t|
+    t.datetime "date"
+    t.string   "workout"
+    t.string   "mood"
+    t.string   "length"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
